681c063ebe5394d97c02c8f52601e9f636dfae1a
Ainhoa Moreno <36454598+aindroid@users.noreply.github.com>
2025-12-02 21:50:28 +0000

    Update API URLs and allowed hosts for Codespace compatibility

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-f91gl!jwr5o*ati&v(1r)=e&1_^f(jq-g(@s3wvb#&qhz9@i)e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    f'{CODESPACE_NAME}-8000.app.github.dev' if CODESPACE_NAME else '*',
+]
 
 
 # Application definition
